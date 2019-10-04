bfca15af5381c8fa2688ad085d9d15f5899c1f4f
miguel <miguelmu@kth.se>
2019-10-04 12:15:48 +0200

    add populate

--- src/pardisLab3/pardisLab3Main.java
+++ src/pardisLab3/pardisLab3Main.java
@@ -1,6 +1,30 @@
 package pardisLab3;
 
+import java.util.Random;
+
 public class pardisLab3Main {
+
+  static void populate(LazySkipList<Integer> lsl) {
+    Random rng = new Random();
+    Thread[] t = new Thread[Runtime.getRuntime().availableProcessors()];
+    for (int i = 0; i < t.length; i++) {
+      t[i] = new Thread() {
+        public void run() {
+          for (int i = 0; i < 100000 / t.length; i++) {
+            lsl.add(rng.nextInt());
+          }
+        }
+      };
+      t[i].start();
+    }
+    for (int i = 0; i < t.length; i++) {
+      try {
+        t[i].join();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
