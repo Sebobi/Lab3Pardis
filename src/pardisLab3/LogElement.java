9d7ed232d291ecff259b4665b9bc40f1bd257338
Sebastian Isheden <sisheden@kth.se>
2019-10-08 18:26:14 +0200

    Logelemtn))

---
+++ src/pardisLab3/LogElement.java
@@ -0,0 +1,27 @@
+package pardisLab3;
+
+public class LogElement implements Comparable<LogElement>{
+	public static final int ADD = 0;
+	public static final int REMOVE = 1;
+	public static final int CONTAINS = 2;
+	
+	public int value;
+	public int operation;
+	public long returnStamp;
+	public boolean result;
+	public LogElement(int value, int operation, long returnStamp, boolean result) {
+		this.value = value;
+		this.operation = operation;
+		this.returnStamp = returnStamp;
+		this.result = result;
+	}
+	@Override
+	public int compareTo(LogElement arg) {
+		if(returnStamp-arg.returnStamp < 0)
+			return -1;
+		else if(returnStamp-arg.returnStamp > 0)
+			return 1;		
+		return 0;
+	}
+	
+}
